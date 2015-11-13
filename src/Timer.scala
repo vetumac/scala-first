1dd0e6bbb49acccaa764229dfc8d635b166b5106
Alexey Suslov <alexei.suslov@instinctools.ru>
2015-11-13 15:04:44 +0300

    Initial commit

---
+++ src/Timer.scala
@@ -0,0 +1,17 @@
+object Timer {
+  def oncePerSecond(callback: () => Unit): Unit = {
+    while (true) {
+      callback()
+      Thread.sleep(1000)
+    }
+  }
+
+  def timeFlies(): Unit = {
+    Console.println("Time flies when you're having fun(ctionally)...");
+  }
+
+  def main(args: Array[String]): Unit = {
+    oncePerSecond(timeFlies)
+  }
+}
+
