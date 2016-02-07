0ce1758bdc5dc225b4f16ec8ca7bd548febbbd8b
Alexey Suslov <alexei.suslov@gmail.com>
2016-02-07 16:12:33 +0300

    Quiksort

---
+++ src/Quicksort.scala
@@ -0,0 +1,8 @@
+class Quicksort {
+  def sort(a: Array[Int]): Array[Int] =
+    if (a.length < 2) a
+    else {
+      val pivot = a(a.length / 2)
+      sort(a filter (pivot >)) ++ (a filter (pivot ==)) ++ sort(a filter (pivot <))
+    }
+}
